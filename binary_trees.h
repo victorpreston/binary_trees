de4cd1c8a713dec12df40aa64cc7ca979ed65f23
victorpreston <prestonvictor25@gmail.com>
2023-11-29 07:27:18 -0800

    binary_trees

--- binary_trees.h
+++ binary_trees.h
@@ -31,5 +31,10 @@ typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s binary_tree_t;
 
 
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+
+
+#endif
